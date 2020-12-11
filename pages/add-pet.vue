ee298a1558f773069b989d734a0ef3126d6ead5c
Edwin González <edgoca2498@gmail.com>
2020-12-11 13:18:50 -0600

    :sparkles: Added new add-pet page

---
+++ pages/add-pet.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem incidunt illum optio quia inventore. Fugiat nostrum dolor maxime atque repellat praesentium sed nesciunt voluptates expedita eaque? Dolorum voluptatibus omnis nam?
+  </div>
+</template>
