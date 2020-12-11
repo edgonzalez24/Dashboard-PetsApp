312c1d9197802ca7e2481eaf1aaa840278cdea82
Edwin González <edgoca2498@gmail.com>
2020-12-11 10:39:52 -0600

    :alien: Added code due to external services and added servermiddleware

---
+++ api/services.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+// EndPoints
+app.post('/pets', async(req, res) => {
+    try {
+        res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+        res.header('Pragma', 'no-cache');
+        const { data } = await axios.post(`${process.env.SERVICES_URL}?token=${process.env.TOKEN}`, req.body);
+        res.json(data);
+    } catch (error) {
+        if (error.response.status) {
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            res.sendStatus(500);
+        }
+    }
+});
+app.get('/getpets', async(req, res) => {
+    try {
+        res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+        res.header('Pragma', 'no-cache');
+        const { data } = await axios.get(`${process.env.BASE_URL}?token=${process.env.TOKEN}`, {
+            params: JSON.parse(req.header('params') || '{}'),
+        });
+        res.json(data);
+    } catch (error) {
+        if (error.response.status) {
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            res.sendStatus(500);
+        }
+    }
+});
+module.exports = {
+    path: '/api/services',
+    handler: app,
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,50 @@
+/* eslint-disable indent */
+require('./config');
+
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'dashboard-pets',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    // Global page headers (https://go.nuxtjs.dev/config-head)
+    server: {
+        host: '0.0.0.0',
+    },
+
+    head: {
+        title: 'Dashboard',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+            {
+                rel: 'preload',
+                href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap',
+                as: 'style',
+                onload: 'this.onload=null;this.rel=\'stylesheet\'',
+            },
+        ]
+    },
+    env: {
+        token: process.env.TOKEN
+    },
+    axios: {
+        credentials: false,
+        proxy: true,
+    },
+    loading: {
+        color: '#005eb0',
+    },
+    pageTransition: 'page',
+    css: ['~/assets/App.scss'],
+    plugins: [],
+    components: true,
+    buildModules: [
+        '@nuxtjs/tailwindcss',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+    modules: [
+        '@nuxtjs/axios',
+        '@nuxtjs/dotenv',
+        '~/modules/api/services',
+    ],
+    build: {}
 }
